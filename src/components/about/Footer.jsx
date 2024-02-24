a50526f446d1a2326c89101a655c44db3176fbd5
TheValour <hellowroldexe18@gmail.com>
2024-02-24 21:33:22 +0530

    Basic Ui changes

--- src/components/about/Footer.jsx
+++ src/components/about/Footer.jsx
@@ -5,18 +5,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; 2023 Space Y</p>
-        <div className="social-icons">
-          <a href="#" className="icon-link">
-            <i className="fab fa-facebook"></i>
-          </a>
-          <a href="#" className="icon-link">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="#" className="icon-link">
-            <i className="fab fa-instagram"></i>
-          </a>
-        </div>
+        <p>&copy; 2024 Space Y</p>
       </div>
     </footer>
   );
--- src/components/destination/Destination.jsx
+++ src/components/destination/Destination.jsx
@@ -1,11 +1,13 @@
 import Moon from './Moon'
 import Mars from './Mars';
+import Footer from '../about/Footer';
 
 export default function Destination() {
   return (
     <div className='dest-container' >
       <Moon />
       <Mars />
+      <Footer/>
     </div>
   )
 }
